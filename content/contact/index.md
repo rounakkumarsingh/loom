a96bbfb2eb5a6a94bde6374363cfde0da9444200
Rounak Kumar Singh <rounak.official.2@gmail.com>
2025-12-18 01:34:51 +0530

    recursive page generation

---
+++ content/contact/index.md
@@ -0,0 +1,9 @@
+# Contact the Author
+
+[< Back Home](/)
+
+Give me a call anytime to chat about Tolkien!
+
+`XXX-XXX-XXXX`
+
+**"Rounak Kumar Singh"**
--- src/main.py
+++ src/main.py
@@ -1,7 +1,8 @@
 import logging
+import os
 from pathlib import Path
 import re
-from website_utils import copy_recursive
+import shutil
 from block import markdown_to_html_node
 
 
@@ -12,7 +13,26 @@ def extract_title(markdown: str) -> str:
     return match.group(1).strip()
 
 
-def generate_page(from_path: str, template_path: str, dest_path: str):
+logger = logging.getLogger(__name__)
+
+
+def copy_recursive(src: Path, dest: Path):
+    logger.info(f"copying {src.absolute()}")
+    if dest.exists() and dest.is_dir():
+        shutil.rmtree(dest)
+    os.mkdir(dest)
+    logger.info(f"Creating dir {dest.absolute()}")
+    for item in os.listdir(src):
+        if (src / item).is_dir():
+            copy_recursive(src / item, dest / item)
+        else:
+            logger.info(
+                f"Copying {(src / item).absolute()} to {(dest / item).absolute()}"
+            )
+            shutil.copy(src / item, (dest / item))
+
+
+def generate_page(from_path: Path, template_path: Path, dest_path: Path):
     print(f"Generating page from {from_path} to {dest_path} using {template_path}")
     markdown = Path(from_path).read_text()
     template = Path(template_path).read_text()
@@ -25,10 +45,36 @@ def generate_page(from_path: str, template_path: str, dest_path: str):
         f.write(template)
 
 
+def generate_page_recursively(
+    dir_path_content: Path, template_path: Path, dest_dir_path: Path
+):
+    dest_dir_path.mkdir(exist_ok=True)
+
+    for entry in dir_path_content.iterdir():
+        if entry.is_dir():
+            generate_page_recursively(
+                entry,
+                template_path,
+                dest_dir_path / entry.name,
+            )
+        elif entry.suffix == ".md":
+            generate_page(
+                entry,
+                template_path,
+                dest_dir_path / (entry.stem + ".html"),
+            )
+
+
 def main():
     logging.basicConfig(level=logging.INFO)
-    copy_recursive(Path("static"), Path("public"))
-    generate_page("content/index.md", "template.html", "public/index.html")
+
+    public = Path("public")
+    if public.exists():
+        shutil.rmtree(public)
+    public.mkdir()
+
+    copy_recursive(Path("static"), public)
+    generate_page_recursively(Path("content"), Path("template.html"), public)
 
 
 if __name__ == "__main__":
